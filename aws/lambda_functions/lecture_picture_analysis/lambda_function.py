4a800c8bfce768b18783a35e501581175bb2630a
Justin Zwick <jaz2130@columbia.edu>
2020-04-21 12:43:48 -0400

    writes to /tmp/ of lambda file system

--- aws/lambda_functions/lecture_picture_analysis/lambda_function.py
+++ aws/lambda_functions/lecture_picture_analysis/lambda_function.py
@@ -7,6 +7,7 @@ QUEUE_URL= os.environ["QUEUE_URL"]
 
 def lambda_handler(event, context):
     # prof_email/classroom_id/date.jpg
+    print("Logging the event: " + str(event))
     s3_object_key = event["Records"][0]["s3"]["object"]["key"]
     s3_resource = boto3.resource('s3')
     date = s3_object_key.split("/")[2][0:-4]
@@ -14,18 +15,18 @@ def lambda_handler(event, context):
     
     # download the file to the local file system for cropping
     my_bucket = s3_resource.Bucket(LECTURE_BUCKET)
-    my_bucket.download_file(KEY, "temp.jpg")
+    my_bucket.download_file(s3_object_key, "/tmp/temp.jpg")
     
-    im = Image.open('temp.jpg')
+    im = Image.open('/tmp/temp.jpg')
     im = im.convert('RGB')
     
     idx = 0
     
     # call detect_faces on the image to get the bounding boxes
-    for face in detect_faces(LECTURE_BUCKET, KEY):
+    for face in detect_faces(LECTURE_BUCKET, s3_object_key):
         boundaries = face['BoundingBox']
         # once we have the boundaries, we need to use that to crop the image
-        im = Image.open('temp.jpg')
+        im = Image.open('/tmp/temp.jpg')
         im = im.convert('RGB')
         width, height = im.size
         left = boundaries["Left"] * width
@@ -33,7 +34,7 @@ def lambda_handler(event, context):
         right = left + (boundaries["Width"] * width)
         bottom = top + (boundaries["Height"] * height)
         idx += 1
-        output = 'test-face-' + str(idx) + '.jpg'
+        output = '/tmp/test-face-' + str(idx) + '.jpg'
         # write the cropped img to an external file
         im.crop((left, top, right, bottom)).save(output, quality=95)
         with open(output, 'rb') as image:
@@ -65,19 +66,19 @@ def search_faces_by_local_image(image_file, collection_id, students, threshold=8
     )
 
     if response['FaceMatches'][0]["Face"].get("ExternalImageId"):
-        student_ids_in_lecture.append(response['FaceMatches'][0]["Face"].get("ExternalImageId"))
+        students.append(response['FaceMatches'][0]["Face"].get("ExternalImageId"))
     else:
         print("FACE IDENTIFIED, BUT WAS NOT INDEXED IN REKOGNITION COLLECTION")
 
 def detect_faces(bucket, key, attributes=['ALL'], region="us-east-1"):
     rekognition = boto3.client("rekognition", region)
     response = rekognition.detect_faces(
-	    Image={
-			"S3Object": {
-				"Bucket": bucket,
-				"Name": key,
-			}
-		},
-	    Attributes=attributes,
-	)
+        Image={
+            "S3Object": {
+                "Bucket": bucket,
+                "Name": key,
+            }
+        },
+        Attributes=attributes,
+    )
     return response['FaceDetails']
