26917d559314400e823fbb770244863220a5b458
Justin Zwick <jaz2130@columbia.edu>
2020-04-21 20:07:45 -0400

    lambda function to create Google Sheet upon classroom creation

---
+++ aws/lambda_functions/create_google_spreadsheet_for_class/lambda_function.py
@@ -0,0 +1,21 @@
+import json, gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+SCOPE = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+CREDS = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', SCOPE)
+
+def lambda_handler(event, context):
+    # TODO implement
+    class_name = event["class_name"]
+    prof_email = event["prof_email"]
+
+    gc = gspread.authorize(CREDS)
+
+    sheet = gc.create(class_name + " attendance")
+
+    sheet.share(prof_email, perm_type='user', role='reader')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({"spreadsheet_url": sheet.url})
+    }
