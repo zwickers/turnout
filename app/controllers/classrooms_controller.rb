45bce1ca5ee5478b4880224e2c83ad6ceeb78065
Justin Zwick <jaz2130@columbia.edu>
2020-04-21 20:04:00 -0400

    classrooms_controller#create executes create_google_spreadsheet lambda

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -14,8 +14,25 @@ class ClassroomsController < ApplicationController
 	# POST /classrooms
 	def create
 		class_name = params[:class_name]
-		Classroom.create(name: class_name, prof_id: current_user.id)
-		redirect_to "/classrooms"
+		# create the Lambda client we use to invoke the Lambda function.
+		client = Aws::Lambda::Client.new(region: 'us-east-1')
+		req_payload = {:prof_email => current_user.email, :class_name => class_name}
+		payload = JSON.generate(req_payload)
+		resp = client.invoke({
+                         function_name: 'create_google_spreadsheet_for_class',
+                         invocation_type: 'RequestResponse',
+                         log_type: 'None',
+                         payload: payload
+                       })
+		resp_payload = JSON.parse(resp.payload.string) 
+		
+		# If the status code is 200, the call succeeded
+		if resp_payload["statusCode"] == 200
+		  body = JSON.parse(resp_payload["body"])
+		  url = body["spreadsheet_url"]
+		  Classroom.create(name: class_name, prof_id: current_user.id, url: url)
+		  redirect_to "/classrooms"
+		end
 	end
 
 	# GET /classroom/id
